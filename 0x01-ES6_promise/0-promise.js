3056d3d0ec8243c997d9c707fcc9db879563b201
paxxchal <paschalgabrielacj8@gmail.com>
2024-07-24 21:16:42 +0100

    pushed task

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+// 0-promise.js
+
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // This Promise doesn't do anything specific yet,
+      // it's just an example of returning a Promise
+      resolve();
+    });
+  }
