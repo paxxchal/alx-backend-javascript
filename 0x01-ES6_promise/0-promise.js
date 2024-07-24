c1d57f0737844a00299ff7fc92e8cb4b4312a3fc
Paschal <59150676+paxxchal@users.noreply.github.com>
2024-07-25 00:03:38 +0100

    completed task :white-check-mark:

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // This Promise doesn't do anything specific yet,
-      // it's just an example of returning a Promise
-      resolve();
-    });
-  }
-  
+  return new Promise(() => {});
+}
