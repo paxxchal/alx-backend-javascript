f205b0ae8d03e95345494243ca53091ceb0de43b
paxxchal <paschalgabrielacj8@gmail.com>
2024-09-20 13:58:08 +0100

    completed a task ✅

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,42 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    readDatabase(process.argv[2])
+      .then((students) => {
+        let output = 'This is the list of our students\n';
+        const sortedFields = Object.keys(students).sort((a, b) => 
+          a.localeCompare(b, undefined, {sensitivity: 'base'}));
+        
+        sortedFields.forEach((field) => {
+          output += `Number of students in ${field}: ${students[field].length}. `;
+          output += `List: ${students[field].join(', ')}\n`;
+        });
+        
+        response.status(200).send(output.trim());
+      })
+      .catch((error) => {
+        response.status(500).send(error.message);
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
+    
+    if (major !== 'CS' && major !== 'SWE') {
+      response.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(process.argv[2])
+      .then((students) => {
+        const studentsInMajor = students[major] || [];
+        response.status(200).send(`List: ${studentsInMajor.join(', ')}`);
+      })
+      .catch((error) => {
+        response.status(500).send(error.message);
+      });
+  }
+}
+
+export default StudentsController;
---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+const readDatabase = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.trim().split('\n');
+        const students = {};
+        
+        lines.slice(1).forEach(line => {
+          const [firstname, , , field] = line.split(',');
+          if (!students[field]) students[field] = [];
+          students[field].push(firstname);
+        });
+
+        resolve(students);
+      }
+    });
+  });
+};
+
+export default readDatabase;
