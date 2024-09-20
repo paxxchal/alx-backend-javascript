c1478580c9c1c21259f0c74c50326f3c1b74ef9d
paxxchal <paschalgabrielacj8@gmail.com>
2024-09-20 13:05:22 +0100

    trial

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,6 @@
-/**
- * Displays a message in STDOUT.
- *
- * @param {string} message - The message to be displayed.
- */
 function displayMessage(message) {
     console.log(message);
-}
+  }
   
-module.exports = displayMessage;
+  module.exports = displayMessage;
+  
